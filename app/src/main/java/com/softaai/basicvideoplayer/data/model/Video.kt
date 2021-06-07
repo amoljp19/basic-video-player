8474a971b8eb291e2fcde778ac07207dd3e1875f
amol pawar <amoljp19@gmail.com>
2021-06-07 10:59:19 +0530

    added video play pause seek by user using binding adapter and viewmodel

--- app/src/main/java/com/softaai/basicvideoplayer/data/model/Video.kt
+++
@@ -1,9 +0,0 @@
-package com.softaai.basicvideoplayer.data.model
-
-import androidx.room.PrimaryKey
-
-data class Video(
-    @PrimaryKey var id: Int,
-    var videoName: String?,
-    var path: String,
-)
